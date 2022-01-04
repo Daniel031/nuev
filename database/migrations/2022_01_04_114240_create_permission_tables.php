22721e34a7c39f5bc3e09d18c96fb4ec4fd2436d
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2022-01-04 08:04:06 -0400

    spatie

--- database/migrations/2022_01_04_114240_create_permission_tables.php
+++ database/migrations/2022_01_04_114240_create_permission_tables.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Contracts\Routing\Registrar;
 
 class CreatePermissionTables extends Migration
 {
